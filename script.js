86f4c88fa0ab9f1d5a9304f701a577122f971ad8
Aaryan162008 <aaryanrajgupta@gmail.com>
2022-02-27 11:35:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity: 0.4,
+    image1: "sasuke image 2.jpg",
+    image2: "sasuke image 1.jpg",
+    displacementImage: "heightMap.png"
+})
